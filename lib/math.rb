80dbdb4377e5c5b6053ac5bf2f8bfd0e8fc7bbe8
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-07 23:35:33 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,12 +9,13 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  result = num1 * num2
+  result = num2 / num1
   return result
 end
 
 def multiplication(num1, num2)
-
+result = num1 * num2
+return result
 end
 
 def modulo(num1, num2)
