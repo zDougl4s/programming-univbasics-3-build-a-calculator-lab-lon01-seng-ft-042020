7c41b42f68ba8532ca81c28934e5e3d772c0c181
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-07 23:42:14 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  result = num2 + num1
+return result
 end
 
 def subtraction(num1, num2)
-
+ result = num1 - num2
+ return result
 end
 
 def division(num1, num2)
-
+  result = num1 / num2
+  return result
 end
 
 def multiplication(num1, num2)
-
+result = num1 * num2
+return result
 end
 
 def modulo(num1, num2)
-
+result = num1 % num2
+return result
 end
 
 def square_root(num)
-
+result = Math.sqrt(num)
+return result
 end
